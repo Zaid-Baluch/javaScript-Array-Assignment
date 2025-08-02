e81b704b67af725ba4bce8486ee8a97e1ce48a9e
Sohaib Raheem <zaidbeluch@gmail.com>
2025-08-02 16:43:25 +0500

    fir-chapter-completed

---
+++ index.js
@@ -0,0 +1,16 @@
+// CHAPTER 14-16       ARRAYS
+
+// QUESTION 1
+
+var studentNames = [];
+
+// QUESTION 2
+
+var studentNames = ();
+
+// QUESTION 3
+
+var fruits = ["Apple", "Banana", "Mango"]
+
+// QUESTION 4
+
